2a95778589a1f037d2845d0c00411d02ccf114f4
babaorere <baba.orere@gmail.com>
2020-10-11 11:17:41 -0300

    Al final del 3 video, funciona el registro de usuario

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(os.getcwd()) + "/myshop.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.abspath(os.getcwd()) + "/myshop.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "lknmvfsdok394805·$%$·%&"
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from shop.admin import routes
